0b4863e37928a5e3ee59bca3a677cdcdfdfd16ee
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-04 16:08:17 +0200

    add exercise 2
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ const sumEveryOther = (...numeros) => numeros.reduce((acc, val) => acc + val, 0)
 
 console.log(sumEveryOther(6, 8, 2, 3, 1)); //20
 console.log(sumEveryOther(11, 3, 12)); //26
+
+/**
+ * Ejercicio 2:
+ * Escribe una función llamada addOnlyNums que pueda recibir cualquier número de argumentos (incluyendo números y strings) y retorne la suma solo de los números.
+ */
+const addOnlyNums = (...elementos) => elementos.filter(elemento => typeof elemento === 'number').reduce((acc, val) => acc + val, 0);
+
+console.log(addOnlyNums(1, "perro", 2, 4)); //7
